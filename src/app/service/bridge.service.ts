2a81ce4d6ce2970e6b3cb02f1e11031850d3dc65
Jimson91 <j.hamshere91@gmail.com>
2020-03-08 17:55:28 +1000

    add ros3d example for prototype

--- src/app/service/bridge.service.ts
+++ src/app/service/bridge.service.ts
@@ -3,6 +3,9 @@ import { Injectable, Input } from '@angular/core';
 declare var ROSLIB: any;
 import 'roslib/build/roslib.js';
 
+declare var ROS3D: any;
+import 'ros3d/build/ros3d.js';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,6 +14,8 @@ export class BridgeService {
 
   private socket: any;
   private listener: any;
+  private viewer: any;
+  private gridClient: any;
   public isConnected: boolean = false;
   constructor() { }
 
@@ -52,10 +57,25 @@ export class BridgeService {
   }
 
   onConnected() {
+    // create a basic ros topic that listens for incoming messages
     this.listener = new ROSLIB.Topic({
       ros: this.socket,
       name: '/listener',
       messageType: 'std_msgs/String'
+    });
+    
+    // create the main 3d viewer.
+    this.viewer = new ROS3D.Viewer({
+      divID: 'map',
+      width: 800,
+      height: 600,
+      antialias: true
+    });
+
+    //Setup the map client.
+    this.gridClient = new ROS3D.OccupancyGridClient({
+      ros: this.socket,
+      rootObject: this.viewer.scene
     });
   }
 
