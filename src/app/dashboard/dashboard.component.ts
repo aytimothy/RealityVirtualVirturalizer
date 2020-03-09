468183381b2c25b3fe7f47491e893059a6082ba7
Jimson91 <j.hamshere91@gmail.com>
2020-03-10 02:46:13 +1000

    add ros3d grid example

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BridgeService } from '../services/bridge.service';
 import { DialogService } from '../services/dialog.service';
+import * as ROS3D from 'ros3d'
 
 @Component({
   selector: 'app-dashboard',
@@ -9,11 +10,13 @@ import { DialogService } from '../services/dialog.service';
 })
 
 export class DashboardComponent implements OnInit {
+  gridClient: any;
   listening: boolean = false;
   messages = [];
+  private viewer: ROS3D.Viewer
   constructor(
     public __BridgeService: BridgeService,
-    private __DialogService: DialogService,
+    private __DialogService: DialogService
   ) { }
 
   ngOnInit() { }
@@ -21,9 +24,23 @@ export class DashboardComponent implements OnInit {
   startListening() {
     this.listening = true;
     this.__BridgeService.onMessage((response: any) => {
-      this.__DialogService.openConfirmDialog("Output: " + response.data).afterClosed().subscribe(confirm => {
-        this.messages.push(response.data);
-      });
+      this.messages.push(response.data);
     });
+  }
+
+  createCanvas() {
+    // create the main 3d viewer.
+    /*this.viewer = new ROS3D.Viewer({
+      divID: 'map',
+      width: 800,
+      height: 600,
+      antialias: true
+    });
+
+    //Setup the map client.
+    this.gridClient = new ROS3D.OccupancyGridClient({
+      ros: this.__BridgeService.socket,
+      rootObject: this.viewer.scene
+    });*/
   }
 }
