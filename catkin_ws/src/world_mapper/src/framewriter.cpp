9b56ad6ccc51b918efc908c5072cc925c151fa9c
Timothy <wentimothychewyu@gmail.com>
2020-03-16 19:24:19 +1000

    Added //todo(s)

--- catkin_ws/src/world_mapper/src/framewriter.cpp
+++ catkin_ws/src/world_mapper/src/framewriter.cpp
@@ -10,16 +10,24 @@ ros::Subscriber* cam_input;
 ros::Subscriber* imu_input;
 ros::Subscriber* laser_input;
 ros::Rate* loop_rate;
+Frame* frame;
 
 void imageCallback (const sensor_msgs::Image::ConstPtr& msg);
 void imuCallback (const sensor_msgs::Imu::ConstPtr& msg);
 void laserCallback (const sensor_msgs::LaserScan::ConstPtr& msg);
+
+void setupSocket();
+void setupWriter();
+void writeFrame(Frame* frame);
+void broadcastFrame(Frame* frame);
 
 int main (int argc, char** argv) {
 	ros::init(argc, argv, "framewriter");
 	nh = new ros::NodeHandle();
 	output = nh->advertise<Frame>("output", 1000);
 	loop_rate = new ros::Rate(10);
+	setupSocket();
+	setupWriter();
 
 	cam_input = n.subscribe("webcam/image_raw", 1000, imageCallback);
 	imu_input = n.subscribe("imu", 1000, imuCallback);
@@ -56,19 +64,23 @@ void imageCallback (const sensor_msgs::Image::ConstPtr& msg) {
 	checkData();
 }
 
+// todo: Work out calibration (should not be necessary since this is done in the Arduino Program)
+double rotAdj = 1;
+double velAdj = 1;
+
 void imuCallback (const sensor_msgs::Imu::ConstPtr& msg) {
 	ros::time now = ros::time.Now();
 	double deltaTime = lastImuTimestamp.toSecs() - now.toSecs();
 	lastImuMessage = msg;
 	lastImuTimestamp = now;
-	double deltaRotX = deltaTime * msg->angularVelocity.x;
-	double deltaRotY = deltaTime * msg->angularVelocity.y;
-	double deltaRotZ = deltaTime * msg->angularVelocity.z;
-
-	// It doesn't work like this, we still neet to transform that along rotation XYZ.
-	double deltaPosX = deltaTime * msg->linearVelocity.x;
-	double deltaPosY = deltaTime * msg->linearVelocity.y;
-	double deltaPosZ = deltaTime * msg->linearvelocity.z;
+	double deltaRotX = deltaTime * msg->angularVelocity.x * rotAdj;
+	double deltaRotY = deltaTime * msg->angularVelocity.y * rotAdj;
+	double deltaRotZ = deltaTime * msg->angularVelocity.z * rotAdj;
+	
+	// todo: Adjust the XYZ coordinate system by the rotation. The orientation isn't always (0, 0, 0).
+	double deltaPosX = deltaTime * msg->linearVelocity.x * velAdj;
+	double deltaPosY = deltaTime * msg->linearVelocity.y * velAdj;
+	double deltaPosZ = deltaTime * msg->linearvelocity.z * velAdj;
 
 	rotX += deltaRotX;
 	rotY += deltaRotY;
@@ -97,6 +109,8 @@ void laserCallback (const sensor_msgs::LaserScan::ConstPtr& msg) {
 	ros::Time now = ros::Time.Now();
 	lastLaserMessage = msg;
 	lastLaserTimestamp = now;
+	
+	// todo: Convert laser values to meters (unless they're already in meters).
 
 	checkData();
 }
@@ -107,12 +121,24 @@ void checkData() {
 }
 
 void createFrame() {
-	// Do whatever to create a frame.
-
+	// todo: Create a frame. (malloc it or something)
+	frame = NULL;	// obviously not
+	
+	writeFrame(frame);
+	broadcastFrame(frame);
+	
 	lastImageMessage = NULL;
 	lastImuMessage = NULL;
 	lastLaserMessage = NULL;
 
 	ros::Time now = ros::Time.now();
 	lastFrameTimestamp = now;
+}
+
+void writeFrame(Frame* frame) {
+	// todo: Use the file writer or writers in setupWriter().
+}
+
+void broadcastFrame(Frame* frame) {
+	// todo: Use the socket server you initialized in setupSocket().
 }
