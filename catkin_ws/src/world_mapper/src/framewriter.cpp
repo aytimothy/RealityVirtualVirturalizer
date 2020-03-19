f5d20a37380e47ebb413ab2cd7f9441414efdff8
rwill86 <41216803+rwill86@users.noreply.github.com>
2020-03-19 15:06:12 +1000

    Fixing Errors & Commenting

--- catkin_ws/src/world_mapper/src/framewriter.cpp
+++ catkin_ws/src/world_mapper/src/framewriter.cpp
@@ -1,3 +1,4 @@
+//Include
 #ifdef _WIN32
 #include "../../../../../../../../../opt/ros/melodic/x64/include/ros/ros.h"
 #include "../../../../../../../../../opt/ros/melodic/x64/include/ros/time.h"
@@ -26,6 +27,10 @@
 #include <stdlib.h> 
 #include <nlohmann/json.hpp>
 
+//Using
+//using json = nlohman::json;
+
+//ROS
 ros::NodeHandle* nh;
 ros::Publisher output;
 ros::Subscriber cam_input;
@@ -34,7 +39,7 @@ ros::Subscriber laser_input;
 ros::Rate* loop_rate;
 ros::Time* lastFrameTimestamp;
 world_mapper::Frame* frame;
-
+//Functions
 void imageCallback (const sensor_msgs::Image::ConstPtr& msg);
 void imuCallback (const sensor_msgs::Imu::ConstPtr& msg);
 void laserCallback (const sensor_msgs::LaserScan::ConstPtr& msg);
@@ -43,25 +48,7 @@ void setupSocket();
 void setupWriter();
 void writeFrame(world_mapper::Frame* frame);
 void broadcastFrame(world_mapper::Frame* frame);
-
-uint32_t seq = 0;
-
-int main (int argc, char** argv) {
-	ros::init(argc, argv, "framewriter");
-	nh = new ros::NodeHandle();
-	output = nh->advertise<world_mapper::Frame>("output", 1000);
-	loop_rate = new ros::Rate(10);
-	setupSocket();
-	setupWriter();
-
-	cam_input = nh->subscribe("webcam/image_raw", 1000, imageCallback);
-	imu_input = nh->subscribe("imu", 1000, imuCallback);
-	laser_input = nh->subscribe("scan", 1000, laserCallback);
-
-	ros::spin();
-	return 0;
-}
-
+//ROS Time 
 ros::Time timestamp;
 void checkData();
 void createFrame();
@@ -81,47 +68,76 @@ double posX = 0.0;
 double posY = 0.0;
 double posZ = 0.0;
 
+uint32_t seq = 0;
+//Main
+int main (int argc, char** argv) {
+	ros::init(argc, argv, "framewriter");
+	nh = new ros::NodeHandle();
+	output = nh->advertise<world_mapper::Frame>("output", 1000);
+	loop_rate = new ros::Rate(10);
+	setupSocket();
+	setupWriter();
+
+	cam_input = nh->subscribe("webcam/image_raw", 1000, imageCallback);
+	imu_input = nh->subscribe("imu", 1000, imuCallback);
+	laser_input = nh->subscribe("scan", 1000, laserCallback);
+
+	ros::spin();
+	return 0;
+}
+//Image Callback
 void imageCallback (const sensor_msgs::Image::ConstPtr& msg) {
 	ros::Time now = ros::Time::now();
 	lastImageMessage = *msg;
 	lastImageTimestamp = now;
-
+    //Call Function
 	checkData();
 }
-
 // todo: Work out calibration (should not be necessary since this is done in the Arduino Program)
 double rotAdj = 1;
 double velAdj = 1;
-
+//IMU Callback
 void imuCallback (const sensor_msgs::Imu::ConstPtr& msg) {
 	ros::Time now = ros::Time::now();
 	double deltaTime = lastImuTimestamp.toSec() - now.toSec();
 	lastImuMessage = msg;
 	lastImuTimestamp = now;
-	double deltaRotX = deltaTime * msg->angularVelocity.x * rotAdj;
-	double deltaRotY = deltaTime * msg->angularVelocity.y * rotAdj;
-	double deltaRotZ = deltaTime * msg->angularVelocity.z * rotAdj;
+	double deltaRotX = deltaTime * msg->angularVelocity->x * rotAdj;
+	double deltaRotY = deltaTime * msg->angularVelocity->y * rotAdj;
+	double deltaRotZ = deltaTime * msg->angularVelocity->z * rotAdj;
 	
-	double deltaPosX = deltaTime * msg->linearVelocity.x * velAdj;
-	double deltaPosY = deltaTime * msg->linearVelocity.y * velAdj;
-	double deltaPosZ = deltaTime * msg->linearvelocity.z * velAdj;
+	double deltaPosX = deltaTime * msg->linearVelocity->x * velAdj;
+	double deltaPosY = deltaTime * msg->linearVelocity->y * velAdj;
+	double deltaPosZ = deltaTime * msg->linearvelocity->z * velAdj;
 
 	rotX += deltaRotX;
 	rotY += deltaRotY;
 	rotZ += deltaRotZ;
-	if (rotX >= 360.0)
-		rotX -= 360.0;
-	if (rotX < 0.0)
-		rotX += 360.0;
-	if (rotY >= 360.0)
-		rotY -= 360.0;
-	if (rotY < 0.0)
-		rotY += 360.0;
-	if (rotZ >= 360.0)
-		rotZ -= 360.0;
-	if (rotZ < 0.0)
-		rotZ += 360.0;
 	
+	if (rotX >= 360.0){
+		rotX -= 360.0;
+    }
+    
+	if (rotX < 0.0){
+		rotX += 360.0;
+    }
+    
+	if (rotY >= 360.0){	
+		rotY -= 360.0;
+    }
+    
+	if (rotY < 0.0){
+		rotY += 360.0;
+    }
+    
+	if (rotZ >= 360.0){
+		rotZ -= 360.0;
+    }
+    
+	if (rotZ < 0.0){
+		rotZ += 360.0;
+    }
+
 	double radX = rotX * (M_PI / 180.0);
 	double radY = rotY * (M_PI / 180.0);
 	double radZ = rotZ * (M_PI / 180.0);
@@ -165,21 +181,27 @@ void imuCallback (const sensor_msgs::Imu::ConstPtr& msg) {
 
 	checkData();
 }
-
+//Laser Callback
 void laserCallback (const sensor_msgs::LaserScan::ConstPtr& msg) {
 	ros::Time now = ros::Time::now();
 	lastLaserMessage = msg;
 	lastLaserTimestamp = now;
-
+    //Call Function
 	checkData();
 }
-
-void checkData() {
-	if (lastImageMessage != NULL && lastImuMessage != NULL && lastLaserMessage != NULL)
-		createFrame();
+//Check Data
+void checkData() { 
+    //Check Messages
+	if (lastImageMessage != NULL && lastImuMessage != NULL && lastLaserMessage != NULL){
+		//Create Frame
+	    createFrame();
+    } else{
+    	//do nothing? 
+	}
 }
-
+//Create Frame 
 void createFrame() {
+	//Init Frame
 	frame = new Frame();
 	frame->accX = lastImuMessage->linear_acceleration->x;
 	frame->accY = lastImuMessage->linear_acceleration->y;
@@ -209,35 +231,41 @@ void createFrame() {
 	
 	writeFrame(frame);
 	broadcastFrame(frame);
-	
+	//Set Messages to NULL
 	lastImageMessage = NULL;
 	lastImuMessage = NULL;
 	lastLaserMessage = NULL;
-
+    //Get Time 
 	ros::Time now = ros::Time::now();
 	lastFrameTimestamp = &now;
 
 	~frame();
 }
-
+//Setup Writer 
 void setupWriter() {
 	// Nothing needed to be done. Just open close files.
 }
-
+//SetupSocket
 void setupSocket() {
-	
+	//
 }
-
+//Write Frame
 void writeFrame(world_mapper::Frame* frame) {
+	//Init JSON
 	std::vector<std::uint8_t> v_bson = json::to_bson(frame);
+	//json j;
+	//Init File
 	std::fstream fs;
 	char filenamebuff[2048];
+	//Open File
 	fs.open(sprintf("frame%5d.bson", filenamebuff, seq));
+	//Write to File
 	fs.write(v_bson.data);
+	//Close to File
 	fs.close();
 }
-
+//Boardcast Frame
 void broadcastFrame(world_mapper::Frame* frame) {
 	// todo: Use the socket server you initialized in setupSocket().
-	
+		
 }
