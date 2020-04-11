3dd6c67f6f5ed1fbac6bf2ef1da3ba489ce0da58
Jimson91 <j.hamshere91@gmail.com>
2020-04-11 23:00:35 +1000

    Add scanning events to the sidenav
    Also update the views accordingly

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,16 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DialogService } from '../services/dialog.service';
+import { BridgeService } from '../services/rosbridge.service';
+import { ScannerService } from '../services/scanner.service';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit {
 
-  constructor(private __DialogService: DialogService) { }
+  isScanning: boolean = false;
+  isConnected: boolean = false;
 
-  configureRosBridge() {
-    this.__DialogService.openRosconfigDialog().afterClosed().subscribe()
+  constructor(
+    private __DialogService: DialogService,
+    public __BridgeService: BridgeService,
+    public __ScannerService: ScannerService
+  ) { }
+
+  public ngOnInit() {
+    // subscribe to connection status
+    this.__BridgeService.getConnnectionStatus().subscribe(status => {
+      this.isConnected = status; // update status in local component when changes
+      if (this.isConnected) {
+        this.__ScannerService.requestScannerStatus();
+      }
+    });
+    // subscribe to scanner status
+    this.__ScannerService.getScannerStatus().subscribe(status => {
+      this.isScanning = status;
+    });
+  }
+
+  public configureRosBridge(): void {
+    this.__DialogService.openRosconfigDialog().afterClosed().subscribe();
+  }
+
+
+  public toggleScanner(): void {
+    this.__ScannerService.toggleScanner(this.isScanning);
   }
 }
