4cf179fe0b0c59396749b8810fa04865710f1fb3
waleed <waleed.jamaldini@griffithuni.edu.au>
2020-04-12 17:07:17 +1000

    updated desktopui

---
+++ Cloud point/Assets/scripts/click_button.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class click_button : MonoBehaviour
+{
+    //when the "BACK" buttom is clicked, this script is invoked to go back to project screen
+    public void click()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Cloud point/Assets/scripts/create_new_project_screen_management.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class create_new_project_screen_management : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    public void back_press()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
