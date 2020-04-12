166482f943821adb3d35848ef005844ffb945fc6
Jimson91 <j.hamshere91@gmail.com>
2020-04-12 23:06:52 +1000

    update build files

--- dist/virtualUI/polyfills-es5.js
+++ dist/virtualUI/polyfills-es5.js
@@ -18899,7 +18899,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
   /***/
   function _(module, exports, __webpack_require__) {
     __webpack_require__(
-    /*! /home/vm-ubuntu/-RealityVirtualVirturalizer-Interface/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js */
+    /*! /home/crypto/Projects/-RealityVirtualVirturalizer-Interface/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js */
     "./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js");
 
     __webpack_require__(
@@ -18907,7 +18907,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
     "./node_modules/zone.js/dist/zone-legacy.js");
 
     module.exports = __webpack_require__(
-    /*! /home/vm-ubuntu/-RealityVirtualVirturalizer-Interface/src/polyfills.ts */
+    /*! /home/crypto/Projects/-RealityVirtualVirturalizer-Interface/src/polyfills.ts */
     "./src/polyfills.ts");
     /***/
   }
