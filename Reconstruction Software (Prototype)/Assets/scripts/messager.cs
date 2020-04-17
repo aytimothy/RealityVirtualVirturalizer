e5887f659d414baf95b86a909855a27f69f069a2
waleed <waleed.jamaldini@griffithuni.edu.au>
2020-04-17 10:38:07 +1000

    updated DesktopUI

---
+++ Reconstruction Software (Prototype)/Assets/scripts/messager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class messager : MonoBehaviour
+{
+    //this script is used to send information between two screens
+
+    //variable used on main menu
+    public static string project_file_path;
+
+    //this variable will store the complete file name of the 
+    public static string file_path;
+
+    //coordinates varables collected on project screen
+    public static int maxX;
+    public static int maxY;
+    public static int maxZ;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
