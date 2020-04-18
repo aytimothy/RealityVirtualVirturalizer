758c1f811616a256561365943fc1dad3c41699f2
Jimson91 <j.hamshere91@gmail.com>
2020-04-19 03:59:10 +1000

    Use three.js renderer over ros3d viewer

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,8 @@
-import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild, Input } from '@angular/core';
 import { BridgeService } from '../services/rosbridge.service';
-import * as ROS3D from 'ros3d';
 import * as THREE from 'three/build/three';
+import *  as  data from '../../assets/Points.json';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,10 +13,11 @@ import * as THREE from 'three/build/three';
 export class DashboardComponent implements AfterViewInit {
 
   @ViewChild('canvas', { read: ElementRef, static: false }) elementView: ElementRef;
+  @Input() material: THREE.PointsMaterial;
+  @Input() geometry: THREE.Geometry;
+
+  private points = [];
   public isConnected: boolean = false;
-
-  private viewer: ROS3D.Viewer
-
   public listeningForMessages: boolean = false;
   public isCanvasDisplayed: boolean = true;
   public isImageDisplayed: boolean = false;
@@ -40,7 +42,9 @@ export class DashboardComponent implements AfterViewInit {
     this.__BridgeService.getConnnectionStatus().subscribe(status => {
       this.isConnected = status
     });
-
+    data.points.forEach(element => {
+      this.points.push(new THREE.Vector3(element.x, element.y, element.z));
+    });
   }
 
   public startListening(): void {
@@ -65,24 +69,57 @@ export class DashboardComponent implements AfterViewInit {
     this.isCanvasDisplayed = false;
   }
 
-  /*private generatePoint(response: any): void {
-   var origin = new Vector3(response.posX, response.posY, response.posZ);
-   var directions = this.generateDirections(new Vector3(response.rotX, response.rotY, response.rotZ));
-   var points = []
-   for (let i = 0; i < response.distances.length; i++)
-       points = origin + (direction[i] * response.distances[i])
-   generateBox(point, 0.0001)
+  /*private generatePoint(frame: any): void {
+    var origin = new THREE.Vector3(frame.posX, frame.posY, frame.posZ);
+    var rotation = new THREE.Vector3(frame.rotX, frame.rotY, frame.rotZ);
+    var directions = this.generateDirections(rotation);
+    this.points = [];
+    for (let i = 0; i < frame.distances.length; i++)
+      this.points = origin + (directions[i] * frame.distances[i])
+    this.generateBox(this.points, 0.0001)
   }
 
-  private generateDirections(x, y, z): THREE.Vector3[] {
-   that stupid rotational matrix thing again
+  private generateDirections(rotation: THREE.Vector3) {
+    // Yaw
+    var cosa = Math.cos(rotation.z);
+    var sina = Math.sin(rotation.z);
+
+    // Pitch
+    var cosb = Math.cos(rotation.y);
+    var sinb = Math.sin(rotation.y);
+
+    // Roll
+    var cosc = Math.cos(rotation.x);
+    var sinc = Math.sin(rotation.x);
+
+    var Axx = cosa * cosb;
+    var Axy = cosa * sinb * sinc - sina * cosc;
+    var Axz = cosa * sinb * cosc + sina * sinc;
+
+    var Ayx = sina * cosb;
+    var Ayy = sina * sinb * sinc + cosa * cosc;
+    var Ayz = sina * sinb * cosc - cosa * sinc;
+
+    var Azx = -sinb;
+    var Azy = cosb * sinc;
+    var Azz = cosb * cosc;
+
+    for (var i = 0; i < this.points.length; i++) {
+      var px = this.points[i].x;
+      var py = this.points[i].y;
+      var pz = this.points[i].z;
+      this.points[i].x = Axx * px + Axy * py + Axz * pz;
+      this.points[i].y = Ayx * px + Ayy * py + Ayz * pz;
+      this.points[i].z = Azx * px + Azy * py + Azz * pz;
+    }
   }
 
-  private generateBox(pos, size): THREE.Object3D {
-   //make points at (1, 1, 1), (1, 1, -1), (1, -1, 1), (1, -1, -1), etc.
-   var 1 = pos + (0.5 * size), -1 = pos - (0.5 * size), for respective axis
-   //join up three four points (or three if we have to make triangles)
-  // add material
+  private generateBox(points, size): THREE.Object3D {
+    //make points at (1, 1, 1), (1, 1, -1), (1, -1, 1), (1, -1, -1), etc.
+    //var l = pos + (0.5 * size) - 1 = pos - (0.5 * size), //for respective axis
+    //join up three four points (or three if we have to make triangles)
+    // add material
+    // MATERIAL
   }*/
 
   public onResize(event: any): void {
@@ -91,11 +128,6 @@ export class DashboardComponent implements AfterViewInit {
     to take into account the two side navigation panels which are both 260*/
     this.dashboardWidth = event.target.innerWidth - 520;
     this.dashboardHeight = event.target.innerHeight - 164;
-
-    // ensure the viewer is created before calling resizing it
-    if (this.viewer != undefined) {
-      this.viewer.resize(this.dashboardWidth, this.dashboardHeight);
-    }
   }
 
   public toggleImages(): void {
@@ -120,15 +152,53 @@ export class DashboardComponent implements AfterViewInit {
 
   public create3DCanvas(): void {
 
-    // create the main 3d viewer.
-    this.viewer = new ROS3D.Viewer({
-      divID: 'canvas',
-      width: this.dashboardWidth,
-      height: this.dashboardHeight,
-      antialias: true
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    camera.position.z = 15;
+    camera.position.y = 5;
+
+    var renderer = new THREE.WebGLRenderer({ antialias: true });
+
+    renderer.setClearColor('#00000');
+    renderer.setSize(this.dashboardWidth, this.dashboardHeight);
+
+    this.elementView.nativeElement.appendChild(renderer.domElement);
+
+    window.addEventListener('resize', () => {
+      renderer.setSize(this.dashboardWidth, this.dashboardHeight);
+      camera.aspect = this.dashboardWidth / this.dashboardHeight;
+      camera.updateProjectionMatrix();
     });
 
-    // create background grid in the viewer
-    this.viewer.addObject(new ROS3D.Grid());
+    // LIGHTS
+    var light = new THREE.PointLight(0xFFFFFF, 1, 500);
+    light.position.set(10, 0, 25);
+    scene.add(light);
+
+    // MATERIAL
+    this.material = new THREE.PointsMaterial({ size: 1, sizeAttenuation: false });
+
+    // GEOMETRY
+    this.geometry = new THREE.Geometry();
+
+    this.points.forEach(element => {
+      this.geometry.vertices.push(element)
+    });
+
+    var mesh = new THREE.Points(this.geometry, this.material);
+    scene.add(mesh);
+
+    var controls = new OrbitControls(camera, renderer.domElement);
+
+    controls.update();
+
+    var render = function () {
+      requestAnimationFrame(render);
+      controls.update();
+      renderer.render(scene, camera)
+    }
+
+    render();
   }
 }
