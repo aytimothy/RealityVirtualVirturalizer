0cf33dd01b8cdedf70f2bc7f57d16b39efc22341
Timothy Chew <wentimothychewyu@gmail.com>
2020-04-23 16:44:11 +1000

    Moved clear buttons from File to Edit

--- Assets/Scripts/Editor/ClearPlayerPrefs.cs
+++ Assets/Scripts/Editor/ClearPlayerPrefs.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 
 public class ClearPlayerPrefs : MonoBehaviour
 {
-    [MenuItem ("File/Clear PlayerPrefs")]
+    [MenuItem ("Edit/Clear PlayerPrefs")]
     public static void ClearPlayerPrefs_OnClick() {
         PlayerPrefs.DeleteAll();
         PlayerPrefs.Save();
     }
 
-    [MenuItem ("File/Clear EditorPrefs")]
+    [MenuItem ("Edit/Clear EditorPrefs")]
     public static void ClearEditorPrefs_OnClick() {
         EditorPrefs.DeleteAll();
     }
