5d757c44c92c670c1cea5929a719e7a14caa5f4e
Timothy Chew <wentimothychewyu@gmail.com>
2020-04-23 16:43:43 +1000

    Added debug button for clearing preferences

---
+++ Assets/Scripts/Editor/ClearPlayerPrefs.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ClearPlayerPrefs : MonoBehaviour
+{
+    [MenuItem ("File/Clear PlayerPrefs")]
+    public static void ClearPlayerPrefs_OnClick() {
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.Save();
+    }
+
+    [MenuItem ("File/Clear EditorPrefs")]
+    public static void ClearEditorPrefs_OnClick() {
+        EditorPrefs.DeleteAll();
+    }
+}
