7bd24c995cc68d095a4e4c36bd82e3facf45ed89
Timothy Chew <wentimothychewyu@gmail.com>
2020-04-24 16:54:13 +1000

    Undid undos in 3270872013c69f3ef6a39f51db21fb6a4cd9de7c

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public float mouseSensitivity = 0.1f;
+    public float walkSpeed = 5f;
+    public float runSpeed = 10f;
+    public bool invertVertical = true;
+    public bool invertHorizontal = false;
+
+    Vector3 mousePosition;
+    Vector3 cameraRotation;
+
+    void Update() {
+        float effectiveMovementSpeed = (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) ? runSpeed : walkSpeed;
+        transform.position += transform.forward * effectiveMovementSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+        transform.position += transform.right * effectiveMovementSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        if (Input.GetKey(KeyCode.Q)) {
+            transform.position += transform.up * effectiveMovementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.E)) {
+            transform.position -= transform.up * effectiveMovementSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1)) {
+            mousePosition = Input.mousePosition;
+            cameraRotation = transform.eulerAngles;
+        }
+        if (Input.GetMouseButton(0) || Input.GetMouseButton(1)) {
+            Vector3 deltaMouse = Input.mousePosition - mousePosition;
+            Vector3 deltaRotation = new Vector3(mouseSensitivity * deltaMouse.y * ((invertVertical) ? -1f : 1f), mouseSensitivity * deltaMouse.x * ((invertHorizontal) ? -1f : 1f), 0f);
+            transform.eulerAngles = cameraRotation + deltaRotation;
+        }
+    }
+}
