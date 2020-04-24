7ae1009a713c7d21a7c2ec34eada7de9799aa734
Timothy Chew <wentimothychewyu@gmail.com>
2020-04-24 11:21:58 +1000

    Assigned events to header menu buttons

--- Assets/Scripts/ProjectViewPanel.cs
+++ Assets/Scripts/ProjectViewPanel.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectViewPanel : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class ProjectViewPanel : MonoBehaviour {
+    public Camera camera;
+
+    public void ViewPointCloudButton_OnClick() {
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void PointCloudImageColorToggle_OnValueChanged(bool newValue) {
+
+    }
+
+    public void PointCloudClassificationColorToggle_OnValueChanged(bool newValue) {
+
+    }
+
+    public void ViewReconstructionButton_OnClick() {
+
+    }
+
+    public void ReconstructionColorToggle_OnValueChanged(bool newValue) {
+
+    }
+
+    public void ReconstructionUVsToggle_OnValueChanged(bool newValue) {
+
+    }
+
+    public void ResetCameraButton_OnClick() {
+        camera.transform.position = Vector3.zero;
+        camera.transform.rotation = Quaternion.identity;
     }
 }
