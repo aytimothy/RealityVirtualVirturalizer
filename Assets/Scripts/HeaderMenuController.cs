9e03bb2745cc637681ed87717d9e516746586982
Timothy <wentimothychewyu@gmail.com>
2020-04-28 17:06:04 +1000

    Deleted everything (For clean override)

--- Assets/Scripts/HeaderMenuController.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-public class HeaderMenuController : PanelController {
-    
-}
--- Assets/Scripts/MainPanelController.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-public class MainPanelController : PanelController {
-    
-}
