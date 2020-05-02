2239435eb5a2f18f27f70e4caa5d4e2ea8d672dc
Timothy <aytimothy@aytimothy.xyz>
2020-05-02 01:51:25 +0000

    Added Start/Stop Scripts

---
+++ catkin_ws/start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Setup Environment and Build Stuffs.
+cd ~/catkin_ws
+# catkin_make
+source ./devel/setup.bash
+
+# Run the stuffs we just built.
+screen -S roscore -d -m roscore
+screen -S videostream -d -m roslaunch video_stream_opencv webcam.launch
+screen -S urgnode -d -m rosrun urg_node urg_node
+screen -S imureader -d -m rosrun world_mapper imureader
+screen -S framewriter -d -m rosrun world_mapper framewriter.py
+screen -S rosbridge -d -m rosrun rosbridge_server rosbridge_websocket
+
+# Start the webserver
+cd ~/GitHub/RealityVirtualVirturalizer/web/-RealityVirtualVirturalizer-Interface
+# npm install
+screen -S webserver -d -m node ./server/server.js
+
+# Return to ROS environment
+cd ~/catkin_ws
+echo Launch Complete!
---
+++ catkin_ws/stop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Kill everything!
+screen -XS roscore quit
+screen -XS framewriter quit
+screen -XS rosbridge quit
+screen -XS urgnode quit
+screen -XS videostream quit
+screen -XS imureader quit
+screen -XS webserver quit
