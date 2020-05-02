c361c00a481b4f8887a997508e477cb9a187aee1
Timothy <aytimothy@aytimothy.xyz>
2020-05-02 02:08:50 +0000

    Added a switch-version of the scripts

---
+++ catkin_ws/switch_start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+screen -S videostream -d -m roslaunch video_stream_opencv webcam.launch
+screen -S urgnode -d -m rosrun urg_node urg_node
+screen -S imureader -d -m rosrun world_mapper imureader
+screen -S framewriter -d -m rosrun world_mapper framewriter.py
---
+++ catkin_ws/switch_stop.sh
@@ -0,0 +1,4 @@
+screen -XS framewriter quit
+screen -XS videostream quit
+screen -XS urgnode quit
+screen -XS imureader quit
