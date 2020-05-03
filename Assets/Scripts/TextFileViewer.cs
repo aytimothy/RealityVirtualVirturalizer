af9040e86addeeec358bc2c89c90f1c12a67ac04
Jimson91 <j.hamshere91@gmail.com>
2020-05-03 16:03:18 +1000

    Set Filename in TextFileViewer.cs

--- Assets/Scripts/TextFileViewer.cs
+++ Assets/Scripts/TextFileViewer.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using System.IO;
+using TMPro.EditorUtilities;
+using UnityEngine.UI;
 
 public class TextFileViewer : MonoBehaviour {
     public TMP_Text Label;
-
+    public string FilePath;
     public void Show(string filePath) {
-        throw new System.NotImplementedException();
+        Label = GetComponent<TMP_Text>();
+        Label.text = Path.GetFileName(filePath);
+        FilePath = filePath;
+        //throw new System.NotImplementedException();
     }
 }
