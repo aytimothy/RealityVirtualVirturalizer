4998c60d7bb932d11bb5b03d0ff9c74a4799dcc9
Timothy Chew <wentimothychewyu@gmail.com>
2020-05-11 22:52:48 +1000

    Added a toggle for full-screen

---
+++ Assets/Scripts/UniversalHotkeys.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UniversalHotkeys : MonoBehaviour {
+    public bool IsFullScreen {
+        get { return PlayerPrefs.GetInt("FullScreen", 0) == 1; }
+        set {
+            PlayerPrefs.SetInt("FullScreen", (value) ? 1 : 0 );
+            PlayerPrefs.Save();
+        }
+    }
+
+    void Awake() {
+        CheckFullscreen();
+
+        void CheckFullscreen() {
+            if (Screen.fullScreen != IsFullScreen)
+                Screen.fullScreen = IsFullScreen;
+        }
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.F11)) {
+            ToggleFullscreen();
+        }
+
+        void ToggleFullscreen() {
+            Screen.fullScreen = !Screen.fullScreen;
+            IsFullScreen = Screen.fullScreen;
+        }
+    }
+}
