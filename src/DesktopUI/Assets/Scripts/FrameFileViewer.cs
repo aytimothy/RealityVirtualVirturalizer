cf75e1d79df857ef73ccbe257ac90100be1d5304
Jimson91 <j.hamshere91@gmail.com>
2020-05-15 00:52:32 +1000

    Render image bytes into UI Image in FrameFileViewer

--- src/DesktopUI/Assets/Scripts/FrameFileViewer.cs
+++ src/DesktopUI/Assets/Scripts/FrameFileViewer.cs
@@ -1,10 +1,9 @@
-using System.IO;
+﻿using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-
 public class FrameFileViewer : MonoBehaviour
 {
     public TextFileViewer TextViewer;
@@ -12,6 +11,7 @@ public class FrameFileViewer : MonoBehaviour
     public TMP_Text FileName;
     public TMP_Text FileContent;
     RectTransform textTransform;
+    public Image ImageContent;
 
     void Update()
     {
@@ -90,27 +90,29 @@ public class FrameFileViewer : MonoBehaviour
         
         FileContent.text = JsonUtility.ToJson(frame, true);*/
 
-        /*try
+      /*try
         {
             var offset = frame.img[10] + frame.img[11] << 8 + frame.img[12] << 16 + frame.img[13] << 24;
             var height = frame.img[22] + frame.img[23] << 8 + frame.img[24] << 16 + frame.img[25] << 24;
             var width = frame.img[18] + frame.img[19] << 8 + frame.img[20] << 16 + frame.img[21] << 24;
-
             Color32[] img_clArray = new Color32[width * height];
-
             for (int i = 0; i < frame.img.Length - offset; i += 4)
             {
                 var img_cl = new Color32(frame.img[offset + i + 0], frame.img[offset + i + 1], frame.img[offset + i + 2], frame.img[offset + i + 3]);
                 img_clArray[i / 4] = img_cl;
             }
-
             Texture2D texture = new Texture2D(200, 200);
             texture.SetPixels32(img_clArray);
-
             imageTemplate.GetComponent<RawImage>().texture = texture;
         }
         catch { }*/
 
+            // Render image from frame.img bytes[] array
+            Texture2D texture = new Texture2D(200, 200);
+            texture.LoadImage(frame.img);
+            Sprite NewSprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0, 0));
+            ImageContent.sprite = NewSprite;
+        
         textTransform = FileContent.GetComponent<RectTransform>();
     }
 }
